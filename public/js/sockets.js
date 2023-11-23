67f2685d237c5f5c9fe42c76439986b9e196b004
Jorge Nava <jorge.nava@boats.com>
2023-11-22 23:09:50 -0700

    Add private chats and WSS

--- public/js/sockets.js
+++ public/js/sockets.js
@@ -1,63 +1,61 @@
-module.exports = function(io){
-
+module.exports = function(io) {
     let users = {};
 
-
     io.on('connection', socket => {
-        
         console.log('new user connected');
 
         socket.on('new user', (data, cb) => {
-            console.log(data);
-            if (users[data] != undefined){
-                cb(false);
-            }else{
-                cb(true);
+            if (users[data]) {
+                cb(false); // Usuario ya existe
+            } else {
+                cb(true); // Nuevo usuario
                 socket.nickname = data;
-                users[socket.nickname]= socket;
-                updateusers();
+                users[socket.nickname] = socket;
+                updateUsers();
             }
         });
 
-        socket.on('send message', (data, cb) =>{
-            
-            var msg = data.trim();
+        socket.on('send message', (data, cb) => {
+            var msg = data.message ? data.message.trim() : '';
 
-            if(msg.substr(0,3) === '/w'){
-                msg =msg.substr(3);
-                const index = msg.indexOf('');
-                if(index !== -1){
-                    var name =msg.substring(0, index);
-                    var msg = msg.substring(index + 1);
-                    if(name in users){
-                        users[name].emit('whisper',{
-                            msg,
+            if (msg.startsWith('/w ')) {
+                msg = msg.slice(3).trim();
+                const spaceIndex = msg.indexOf(' ');
+
+                if (spaceIndex > 0) {
+                    var recipient = msg.substring(0, spaceIndex);
+                    var msgBody = msg.substring(spaceIndex + 1);
+
+                    if (users[recipient]) {
+                        users[recipient].emit('whisper', {
+                            msg: msgBody,
                             nick: socket.nickname,
-                            
+                            time: data.time
                         });
-                    }else{
-                        cb('Error, please enter a valid user');
+                        cb(`Message sent to ${recipient}`);
+                    } else {
+                        cb('Error: please enter a valid user');
                     }
-
-                }else{
-                    cb('Error, please enter your message');
+                } else {
+                    cb('Error: please enter your message');
                 }
-            }else{
+            } else {
                 io.emit('new message', {
-                    msg:data,
-                    nick: socket.nickname });
+                    msg: msg,
+                    nick: socket.nickname,
+                    time: data.time
+                });
             }
         });
-        
-            
-        socket.on('disconnect', data => {
-            if(!socket.nickname) return;
+
+        socket.on('disconnect', () => {
+            if (!socket.nickname) return;
             delete users[socket.nickname];
-            updateusers();
+            updateUsers();
         });
 
-        function updateusers(){
-            io.sockets.emit('usernames',Object.keys(users));
+        function updateUsers() {
+            io.sockets.emit('usernames', Object.keys(users));
         }
     });
 }
